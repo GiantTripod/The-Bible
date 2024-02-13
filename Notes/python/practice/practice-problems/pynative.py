56434bca1c7fff3723253a195867bffbf40a9c83
GiantTripod <chris.mcgrath11@gmail.com>
2024-02-13 09:53:30 -0700

    pynative exercises

---
+++ Notes/python/practice/practice-problems/pynative.py
@@ -0,0 +1,36 @@
+"""Exercise 1: Calculate the multiplication and sum of two numbers
+Given two integer numbers, return their product only if the product is equal to or lower than 1000. Otherwise, return their sum."""
+
+
+
+# def multiadd(x, y):
+#     if x*y > 1000:
+#         print(x*y)
+#     else:
+#         print(x+y)
+
+
+"""Exercise 2: Print the sum of the current number and the previous number
+Write a program to iterate the first 10 numbers, and in each iteration, print the sum of the current and previous number."""
+
+# multiadd(int(input('enter number ')), int(input('enter another number ')))
+
+# for x in range(int(input()), int(input())):
+#     currentnumb = x
+#     previousnumb = x - 1
+#     if previousnumb < 0:
+#         previousnumb = 0
+#     sum = currentnumb + previousnumb
+#     print(f'current number is: {currentnumb} Previous number is: {previousnumb} and the sum is: {sum}')
+
+
+"""Exercise 3: Print characters from a string that are present at an even index number
+Write a program to accept a string from the user and display characters that are present at an even index number."""
+
+# string = input('longest word... ')
+
+# for x in range(len(string)):
+#     if x % 2 == 0:
+#         print(string[x])
+#     else:
+#         pass
